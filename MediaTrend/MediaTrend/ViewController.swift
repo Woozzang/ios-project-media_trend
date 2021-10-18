5f2aea7f1ab1853648ac59706ff3f4f5ff3345d6
Woozzang <woozzaang@gmail.com>
2021-10-19 06:59:09 +0900

    Project Initial Setup

---
+++ MediaTrend/MediaTrend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaTrend
+//
+//  Created by Woochan Park on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
